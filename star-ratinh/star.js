79b9a3b178ecf6be8ed997d784c3a1a18c517e9b
dpfla <dpfla73@gmail.com>
2022-11-10 18:42:06 +0900

    회의

--- star-ratinh/star.js
+++
@@ -1,6 +0,0 @@
-window.onload = function () {
-  $('.rate-area > span').click(function(){
-      $(this).siblings().removeClass('checked');
-      $(this).addClass('checked');
-  });
-}
--- star-ratinh/star1.js
+++
@@ -1,9 +0,0 @@
-$('.starRating span').click(function(){
-  $(this).siblings().removeClass('active');
-  $(this).addClass('active');
-  $(this).parent().addClass('starRated');
-  
-  // Added for Demo
-  let rating = $(this).index() + 1;
-  $('#currentRating').html( "<small>Rating: <b>" + rating + "</b></small>" );
-});
