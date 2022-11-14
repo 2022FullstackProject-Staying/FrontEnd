c5f40d17b60cbb4ab82dee980bcf116d2a9bf9eb
KSY0606 <98162336+KSY0606@users.noreply.github.com>
2022-11-14 14:29:39 +0900

    main
    Signed-off-by: KSY0606 <98162336+KSY0606@users.noreply.github.com>

--- js/member/login.js
+++ js/member/login.js
@@ -1,13 +1,13 @@
-window.onload = function () {
-    $(".login").click(stay_contents_work);
-
-    function stay_contents_work(){
-        let index = $(this).index();
-        console.log(index);
-        $(".login").css("z-index", 0);
-        $(".login").find("h2").removeClass("target");
-        $(".login").eq(index).css("z-index", 10);
-        $(".login").eq(index).find("h2").addClass("target");
-    }
-
+function loginCheck(){
+	if (document.login_form.id.value == ""){
+		alert("아이디를 입력해 주세요.");
+		document.login_form.id.focus();
+		return false;
+	} else if (document.login_form.pwd.value == ""){
+		alert("비밀번호를 입력해주세요.");
+		document.login_form.pwd.focus();
+		return false;
+	} else {
+		return true;
+	}
 }
