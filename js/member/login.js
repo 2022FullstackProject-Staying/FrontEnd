f81ebda6d09ca1e79066dd43263447fbcb3fd09f
dpfla <49601973+dpfla@users.noreply.github.com>
2022-11-16 14:41:27 +0900

    탭 클릭 추가
    Signed-off-by: dpfla <49601973+dpfla@users.noreply.github.com>

--- js/member/login.js
+++ js/member/login.js
@@ -1,13 +1,38 @@
-function loginCheck(){
-	if (document.login_form.id.value.trim() == "") {
+window.onload = function () {
+    $(".login").click(stay_contents_work);
+
+}
+
+function stay_contents_work(){
+	let index = $(this).index();
+	$(".login").css("z-index", 0);
+	$(".login").find("h2").removeClass("target");
+	$(".login").eq(index).css("z-index", 10);
+	$(".login").eq(index).find("h2").addClass("target");
+}
+
+function member_loginCheck(){
+	if (document.member_login_form.id.value == ""){
 		alert("아이디를 입력해 주세요.");
-		document.login_form.id.value = "";
-		document.login_form.id.focus();
+		document.member_login_form.id.focus();
 		return false;
-	} else if (document.login_form.pwd.value.trim() == "") {
+	} else if (document.member_login_form.pwd.value == ""){
 		alert("비밀번호를 입력해주세요.");
-		document.login_form.pwd.value = "";
-		document.login_form.pwd.focus();
+		document.member_login_form.pwd.focus();
+		return false;
+	} else {
+		return true;
+	}
+}
+
+function business_loginCheck(){
+	if (document.business_login_form.id.value == ""){
+		alert("아이디를 입력해 주세요.");
+		document.business_login_form.id.focus();
+		return false;
+	} else if (document.business_login_form.pwd.value == ""){
+		alert("비밀번호를 입력해주세요.");
+		document.business_login_form.pwd.focus();
 		return false;
 	} else {
 		return true;
